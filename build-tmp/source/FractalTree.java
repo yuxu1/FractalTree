82a61440161a45844fcaa849ec23535425060d02
yuxu1 <yufanx8@gmail.com>
2015-01-22 10:46:54 -0800

    Added code for a fractal tree and snow in FractalTree.pde and changed the title, header, and footer in index.html

--- build-tmp/source/FractalTree.java
+++ build-tmp/source/FractalTree.java
@@ -1,7 +1,23 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class FractalTree extends PApplet {
+
 Snowflake[] snow;
-private double fractionLength = .8; 
+private double fractionLength = .8f; 
 private int smallestBranch = 8; 
-private double branchAngle = .7;  
+private double branchAngle = .7f;  
 
 public void setup() 
 {   
@@ -53,11 +69,11 @@ class Snowflake
     x=(int)(Math.random()*700);
     y=(int)(Math.random()*600);
   }
-  void move()
+  public void move()
   {
       y=y+(int)(Math.random()*8+1);
   }
-  void wrap()
+  public void wrap()
   {
     if ((y+6)>=600)
     {
@@ -65,12 +81,21 @@ class Snowflake
       x=(int)(Math.random()*600);
     }
   }
-  void show()
+  public void show()
   {
   	noStroke();
     fill(255,255,255,(int)(Math.random()*255));
     smooth();
     int sizeA = (int)(Math.random()*5+5);
     ellipse(x, y, sizeA, sizeA);
+  }
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "FractalTree" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
   }
 }
